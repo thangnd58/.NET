c303cc5230c137e80582451b8af83e52c81fac9a
thangnd <thangndhe151348@fpt.edu.vn>
2022-06-20 16:20:36 +0700

    commit

---
+++ StoryWeb/StoryWeb/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+namespace StoryWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+
+            public string Index()
+            {
+                return "This is my default action...";
+            }
+
+            public string Welcome()
+            {
+                return "This is the Welcome action method...";
+            }
+    }
+}
---
+++ StoryWeb/StoryWebMVC/Controllers/Helloworld.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace StoryWebMVC.Controllers
+{
+    public class Helloworld : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int numTimes)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
