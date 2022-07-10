eee8902ab3d75db2b3abf9b19f330290121c5581
thangnd <thangndhe151348@fpt.edu.vn>
2022-07-10 13:30:22 +0700

    update add chapter function

--- BookStory/BookStory/Controllers/AdminController.cs
+++ BookStory/BookStory/Controllers/AdminController.cs
@@ -94,7 +94,7 @@ namespace BookStory.Controllers
         [HttpPost]
         public IActionResult AddAuthor(string author)
         {
-            Author a = new ()
+            Author a = new()
             {
                 Aid = context.Authors.ToList().Count + 1,
                 Name = author
@@ -108,7 +108,7 @@ namespace BookStory.Controllers
         [HttpPost]
         public IActionResult AddStory(string name, int[] categories, int author, int status, string source, string image, string keyword, string description)
         {
-            Story story = new ()
+            Story story = new()
             {
                 Name = name,
                 Status = status,
@@ -124,7 +124,7 @@ namespace BookStory.Controllers
             context.SaveChanges();
             for (int i = 0; i < categories.Length; i++)
             {
-                StoriesCategory sc = new ()
+                StoriesCategory sc = new()
                 {
                     Scid = context.StoriesCategories.OrderByDescending(x => x.Scid).FirstOrDefault().Scid + 1,
                     Cid = categories[i],
@@ -133,7 +133,7 @@ namespace BookStory.Controllers
                 context.Add<StoriesCategory>(sc);
                 context.SaveChanges();
             }
-            StoriesAuthor sa = new ()
+            StoriesAuthor sa = new()
             {
                 Said = context.StoriesAuthors.OrderByDescending(x => x.Said).FirstOrDefault().Said + 1,
                 Sid = context.Stories.FirstOrDefault(x => x.Name.Equals(story.Name)).Sid,
@@ -145,10 +145,52 @@ namespace BookStory.Controllers
         }
 
         [HttpPost]
-        public IActionResult AddChapter()
+        public IActionResult EditStory(int sid, string name, int[] categories, int author, int status, string source, string image, string keyword, string description)
         {
+            var entity = context.Stories.FirstOrDefault(x => x.Sid == sid);
+            entity.Name = name;
+            entity.Source = source;
+            entity.Image = image;
+            entity.Status = status;
+            entity.Keyword = keyword;
+            entity.Description = description;
+            entity.UpdatedAt = System.DateTime.Now;
+            context.Entry(entity).CurrentValues.SetValues(entity);
+            var entity1 = context.StoriesAuthors.FirstOrDefault(x => x.Sid == sid);
+            entity1.Aid = author;
+            context.Entry(entity1).CurrentValues.SetValues(entity1);
+            context.StoriesCategories.RemoveRange(context.StoriesCategories.Where(x => x.Sid == sid).ToList());
+            context.SaveChanges();
+            for (int i = 0; i < categories.Length; i++)
+            {
+                StoriesCategory sc = new()
+                {
+                    Scid = context.StoriesCategories.OrderByDescending(x => x.Scid).FirstOrDefault().Scid + 1,
+                    Cid = categories[i],
+                    Sid = sid
+                };
+                context.Add<StoriesCategory>(sc);
+                context.SaveChanges();
+            }
+            return RedirectToAction("ListStory", "Admin");
+        }
 
-            return RedirectToAction("Add", "Admin");
+
+        [HttpPost]
+        public IActionResult AddChapter(int sid, string chaptername, string chapternumber, string chaptersubname, string chaptercontent)
+        {
+            Chapter c = new()
+            {
+                Name = chaptername,
+                Subname = chaptersubname,
+                Chapnumber = chapternumber,
+                Content = chaptercontent,
+                Sid = sid,
+                CreatedAt = System.DateTime.Now
+            };
+            context.Add<Chapter>(c);
+            context.SaveChanges();
+            return RedirectToAction("ListStory", "Admin");
         }
 
         [HttpGet]
@@ -156,6 +198,8 @@ namespace BookStory.Controllers
         {
             Story s = context.Stories.FirstOrDefault(x => x.Sid == storyid);
             Author a = context.Authors.FirstOrDefault(x => x.Aid == x.StoriesAuthors.FirstOrDefault(sa => sa.Sid == storyid).Aid);
+            List<int> sc = context.StoriesCategories.Where(x => x.Sid == storyid).Select(x => x.Cid).ToList();
+            int[] sa = sc.ToArray();
             return Json(new
             {
                 sid = s.Sid,
@@ -166,8 +210,29 @@ namespace BookStory.Controllers
                 keyword = s.Keyword,
                 description = s.Description,
                 authorid = a.Aid,
-                authorname = a.Name
+                authorname = a.Name,
+                listcid = sa,
+            });
+        }
 
+        [HttpGet]
+        public JsonResult GetChapter(int sid)
+        {
+            Chapter s = context.Chapters.OrderByDescending(x => x.Ctid).FirstOrDefault(x => x.Sid == sid);
+            if (s == null)
+            {
+                s = new()
+                {
+                    Sid = sid,
+                    Name = context.Stories.FirstOrDefault(x => x.Sid == sid).Name,
+                    Chapnumber = "0",
+                };
+            }
+            int cnumber = int.Parse(s.Chapnumber) + 1;
+            return Json(new
+            {
+                chapnumber = cnumber,
+                name = s.Name
             });
         }
     }
